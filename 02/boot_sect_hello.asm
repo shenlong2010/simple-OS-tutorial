4935641e6d0e15744714d5e7a34d7b4378a500b0
Loc Lam <shenlong3010@gmail.com>
2021-10-15 00:28:09 -0400

    Add OS barebones and first hello

---
+++ 02/boot_sect_hello.asm
@@ -0,0 +1,15 @@
+mov ah, 0x0e	; tty mode
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10	; 'l' is still on a1, remember?
+mov al, 'o'
+int 0x10
+
+jmp $	; jump to current addr = infinite loop
+; padding and magic number
+times 510 - ($-$$) db 0
+dw 0xaa55
